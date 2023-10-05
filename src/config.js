bc315c3af534b3f72d8055c5e22273984e207f04
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-10-05 22:54:25 +0200

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPMZPbacBRCXdQjyeZ3fcfXWO3Fy_gs6U",
+  authDomain: "to-do-list-80b78.firebaseapp.com",
+  projectId: "to-do-list-80b78",
+  storageBucket: "to-do-list-80b78.appspot.com",
+  messagingSenderId: "619035019868",
+  appId: "1:619035019868:web:f36a48d668f5ca3e7eb499",
+  databaseURL:
+    "https://to-do-list-80b78-default-rtdb.europe-west1.firebasedatabase.app",
+};
--- src/markup/index.js
+++ src/markup/index.js
@@ -1,9 +1,7 @@
 export function createMarkup(array = []) {
   return array
     .map((el) => {
-      return `<li class="item ${el.checked ? "checked" : ""} " data-id="${
-        el.id
-      }">
+      return `<li class="item ${el.done ? "checked" : ""} " data-id="${el.id}">
     <p class="text">${el.value}</p>
     <button type="button" class="button">x</button>
   </li> `;
