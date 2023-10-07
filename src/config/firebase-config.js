e2bdc70f09a0db5ee02e658db72f6f4411113122
Oleg Kovriga <olegkovrigadev@gmail.com>
2023-10-07 10:39:25 +0300

    Add Firebase-config

---
+++ src/config/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB8PmyIdGcITMcx6pWpqp8E79yTzIIwY_4',
+  authDomain: 'todo-list-c4509.firebaseapp.com',
+  projectId: 'todo-list-c4509',
+  storageBucket: 'todo-list-c4509.appspot.com',
+  messagingSenderId: '763595410046',
+  appId: '1:763595410046:web:6684ae74e1e6b97682662f',
+  databaseURL:'https://todo-list-c4509-default-rtdb.europe-west1.firebasedatabase.app',
+};
+
+export default firebaseConfig;
